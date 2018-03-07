871cfc8626eeada6b31506bb1185d5d6e0da6ec7
xp <xp@CETYSDAM017>
2018-03-07 16:13:56 +0100

    Ejercicios acabados nivel 1

--- src/codigo/Nivel1.java
+++ src/codigo/Nivel1.java
@@ -5,125 +5,154 @@
  */
 package codigo;
 
+import java.util.Arrays;
+
 /**
  *
- * @author leo_e
+ * @author Javi Elvira
  */
 public class Nivel1 {
     
-    public int[] maximo(int[] _listaNumeros) {
-	int[] numeroMaximo = {0, 0};
-	for (int i = 0; i < _listaNumeros.length; i++) {
-	    if (numeroMaximo[0] < _listaNumeros[i]) {
-		numeroMaximo[1] = numeroMaximo[0];
-		numeroMaximo[0] = _listaNumeros[i];
-	    } else if (numeroMaximo[1] < _listaNumeros[i]) {
-		numeroMaximo[1] = _listaNumeros[i];
-	    }
-	}
-	return numeroMaximo;
+    public int[] Maximo(int [] listaNumeros){
+        int [] numerosMaximos = {0, 0};
+        
+        for(int i=0; i < listaNumeros.length; i++){
+            if(numerosMaximos[0] < listaNumeros[i]){
+                numerosMaximos[1] = numerosMaximos[0];
+                numerosMaximos[0] = listaNumeros[i];
+            }
+            else if (numerosMaximos[1] < listaNumeros[i]){
+                numerosMaximos[1] = listaNumeros[i];
+            }
+        }
+        return numerosMaximos;
     }
     
-    public String palindromo(String _frase) {
-	//Inicializamos los valores
-	boolean palin = true;
+    public String Palindromo(String Frase) {
+	boolean palindromo = true;
 
-	//Formateamos el String _frase
-	_frase = _frase.toUpperCase();
-	_frase = _frase.replaceAll("Á", "A");
-	_frase = _frase.replaceAll("É", "E");
-	_frase = _frase.replaceAll("Í", "I");
-	_frase = _frase.replaceAll("Ó", "O");
-	_frase = _frase.replaceAll("Ú", "U");
-	_frase = _frase.replaceAll("Ñ", "n");
-	_frase = _frase.replaceAll("\\W", "");
+	Frase = Frase.replaceAll("Á", "A");
+	Frase = Frase.replaceAll("É", "E");
+	Frase = Frase.replaceAll("Í", "I");
+	Frase = Frase.replaceAll("Ó", "O");
+	Frase = Frase.replaceAll("Ú", "U");
+	Frase = Frase.replaceAll("Ñ", "n");
+	Frase = Frase.replaceAll("\\W", "");
 
-	//Comprobamos si la frase es un palíndromo
-	for (int i = 0; i < _frase.length() / 2; i++) {
-	    if (_frase.charAt(i) == _frase.charAt(_frase.length() - i - 1)) {
+	for (int i = 0; i < Frase.length() / 2; i++) {
+	    if (Frase.charAt(i) == Frase.charAt(Frase.length() - i - 1)) {
 	    } else {
-		palin = false;
+		palindromo = false;
 	    }
 	}
 
-	//Devolvemos si es un palíndromo o no
-	if (palin) {
-	    return "Es un palíndromo";
+	//Te dice si es un palíndromo
+	if (palindromo) {
+	    return "Palindromo ok";
 	} else {
-	    return "No es un palíndromo";
+	    return "No palíndromo";
 	}
     }
 
-    public String isograma(String _frase) {
-	boolean isog = true;
+    public String Isograma(String Frase) {
+	boolean isograma = true;
 
-	//Formateamos el String _frase
-	_frase = _frase.toUpperCase();
-	_frase = _frase.replaceAll("Á", "A");
-	_frase = _frase.replaceAll("É", "E");
-	_frase = _frase.replaceAll("Í", "I");
-	_frase = _frase.replaceAll("Ó", "O");
-	_frase = _frase.replaceAll("Ú", "U");
-	_frase = _frase.replaceAll("Ñ", "n");
-	_frase = _frase.replaceAll("\\W", "");
+	Frase = Frase.replaceAll("Á", "A");
+	Frase = Frase.replaceAll("É", "E");
+	Frase = Frase.replaceAll("Í", "I");
+	Frase = Frase.replaceAll("Ó", "O");
+	Frase = Frase.replaceAll("Ú", "U");
+	Frase = Frase.replaceAll("Ñ", "n");
+	Frase = Frase.replaceAll("\\W", "");
 
-	for (int i = 0; i < _frase.length(); i++) {
-	    for (int j = 0; j < _frase.length(); j++) {
-		if (_frase.charAt(i) != _frase.charAt(j) || i == j) {
+	for (int i = 0; i < Frase.length(); i++) {
+	    for (int j = 0; j < Frase.length(); j++) {
+		if (Frase.charAt(i) != Frase.charAt(j) || i == j) {
 		} else {
-		    isog = false;
+		    isograma = false;
 		}
 	    }
 	}
-	if (isog) {
-	    return "Es un isograma";
+        
+        //Te dice si es un isograma
+	if (isograma) {
+	    return "Isograma ok";
 	} else {
-	    return "No es un isograma";
+	    return "No isograma";
 	}
     }
     
-    public void calendario(int _diasemana) {
-	//Inicializamos los valores
+    public void Calendario(int diaSemana) {
 	int filas = 5;
 	int contador = 0;
 	int contador2 = 0;
-	String dianulo = "XX";
-	if (_diasemana >= 5) { //Si la semana empieza despues del jueves
-	    filas = 6; //Añade otra fila
+	String diaNulo = "XX";
+        //Si la semana empieza despues de la resaca del jueves de pacha
+	if (diaSemana >= 5) {
+	    filas = 6;
 	}
-	String[] calend = new String[filas * 7]; //Crea el array de strings
+	String[] calendario = new String[filas * 7];
 
-	//Asigna el valor XX a los dias que esten antes del primer dia
-	for (int i = 0; i <= _diasemana - 2; i++) {
-	    calend[contador] = dianulo;
+	//XX a los dias de antes de que empiece el mes
+	for (int i = 0; i <= diaSemana - 2; i++) {
+	    calendario[contador] = diaNulo;
 	    contador++;
 	}
 
-	//Asigna los números de los dias hasta el 31
+	//Asigna numeros a los dias
 	for (int j = 1; j < 32; j++) {
 	    String aux = Integer.toString(j);
 	    if (j <= 9) {
 		aux = "0" + aux;
 	    }
-	    calend[contador] = aux;
+	    calendario[contador] = aux;
 	    contador++;
 	}
 
-	//Asigna el valor XX a los dias que esten despues del ultimo dia
-	while (contador < calend.length) {
-	    calend[contador] = dianulo;
+	//XX a los dias despues de que acabe el mes
+	while (contador < calendario.length) {
+	    calendario[contador] = diaNulo;
 	    contador++;
 	}
 
 	//Imprime la tabla
 	for (int l = 0; l < filas; l++) {
 	    for (int k = 1; k < 7; k++) {
-		System.out.print(calend[contador2] + " ");
+		System.out.print(calendario[contador2] + " ");
 		contador2++;
 	    }
-	    System.out.print(calend[contador2] + " ");
+	    System.out.print(calendario[contador2] + " ");
 	    System.out.println();
 	    contador2++;
 	}
     }
+    
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Nivel1 ejercicio1 = new Nivel1();
+
+//        int[] listaNumeros = {50, 31, 27, 2, 5, 99};
+//        int[] listaNumeros2 = {150, 31, 27, 2, 5, 99};
+//        System.out.println(Arrays.toString(ejercicio1.Maximo(listaNumeros)));
+//        System.out.println(Arrays.toString(ejercicio1.Maximo(listaNumeros2)));
+
+//        String palindromo = "ANA";
+//        String palindromo2 = "ACASO HUBO BUHOS ACA";
+//        String palindromo3 = "PENE";
+//        System.out.println(ejercicio1.Palindromo(palindromo));
+//        System.out.println(ejercicio1.Palindromo(palindromo2));
+//        System.out.println(ejercicio1.Palindromo(palindromo3));
+
+//        String isograma  = "HOLA";
+//        String isograma2 = "JAVI";
+//        String isograma3 = "PENE";
+//        System.out.println(ejercicio1.Isograma(isograma));
+//        System.out.println(ejercicio1.Isograma(isograma2));
+//        System.out.println(ejercicio1.Isograma(isograma3));
+
+//        ejercicio1.Calendario(3);
+//        System.out.println("");
+}
 }
